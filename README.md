93ad19438809faa80431ba1483de12fe9dee53b9
Chris Carpentier <coredump@gmail.com>
2019-10-15 12:25:32 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Crypt::Paseto - Blah blah blah
+
+# SYNOPSIS
+
+    use Crypt::Paseto;
+
+# DESCRIPTION
+
+Crypt::Paseto is
+
+# AUTHOR
+
+Chris Carpentier <coredump@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2019- Chris Carpentier
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Crypt/Paseto.pm
@@ -0,0 +1,39 @@
+package Crypt::Paseto;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Crypt::Paseto - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Crypt::Paseto;
+
+=head1 DESCRIPTION
+
+Crypt::Paseto is
+
+=head1 AUTHOR
+
+Chris Carpentier E<lt>coredump@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2019- Chris Carpentier
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
